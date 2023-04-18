c32cab48d150b5ee26526f4628755cb68620ece6
elijah obara <elijahobara357@gmail.com>
2023-04-18 19:40:53 +0300

    Admin customizing

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
 from .models import User
 
-class CustomCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
         model = User
         fields = ["email", "username", "first_name", "last_name"]
         error_class = "error"
         
         
-class CustomCreationForm(UserChangeForm):
+class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = User
         fields = ["email", "username", "first_name", "last_name"]
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -6,3 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('supersecret/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin POratl"
+admin.site.index_title = "Welcome to the the REal Estate Poratl"
