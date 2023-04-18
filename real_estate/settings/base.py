dc2ebf613d97642c14e671825dd00115f2193869
elijah obara <elijahobara357@gmail.com>
2023-04-18 23:45:56 +0300

    Signals and debugging

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -147,34 +147,34 @@ logger = logging.getLogger(__name__)
 
 LOG_LEVEL = "INFO"
 
-logging.config.dictConfig({
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "console":{
-            "format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+logging.config.dictConfig(
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "console": {
+                "format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
+            },
+            "file": {"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+            "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
         },
-        "file":{"format": "%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
-        "django.server": DEFAULT_LOGGING["formatters"]["django.server"],
-    },
-    "handlers":{
-        "console":{
-            "class":"logging.StreamHandler",
-            "formatter":"console",
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+                "formatter": "console",
+            },
+            "file": {
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "formatter": "file",
+                "filename": "logs/real_estate.log",
+            },
+            "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
         },
-        "file":{
-            "level":"INFO",
-            "class":"logging.FileHandler",
-            "formatter":"file",
-            "filename":"logs/real_estate.log",
+        "loggers": {
+            "": {"level": "INFO", "handlers": ["console", "file"], "propagate": False},
+            "apps": {"level": "INFO", "handlers": ["console"], "propagate": False},
+            "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
         },
-         "django.server": DEFAULT_LOGGING["handlers"]["django.server"],
-    },
-    "loggers":{
-        "":{"level": "INFO", "handlers":["console", "file"], "propagate":False},
-        "apps":{
-            "level": "INFO", "handlers":["console"], "propagate":False
-        },
-        "django.server": DEFAULT_LOGGING["loggers"]["django.server"],
     }
-})
+)
