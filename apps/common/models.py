bafa82ac285c7cee356895841836d3587444c596
elijah obara <elijahobara357@gmail.com>
2023-04-18 19:54:02 +0300

    Created an abstract model

--- apps/common/models.py
+++ apps/common/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+#used as a base class for other models. Any model that inherits from TimeStampedUUIDModel will automatically inherit its fields and functionality, including the auto-generated primary key and unique UUID
+class TimeStampedUUIDModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
