57f272a6f5219ed6c8bfe1e189ffadc3146191f0
elijah obara <elijahobara357@gmail.com>
2023-04-19 13:28:08 +0300

    property views and registered to admin

--- apps/properties/admin.py
+++ apps/properties/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Property, PropertyViews
 
-# Register your models here.
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ["title", "country", "advert_type", "property_type"]
+    list_filter = ["advert_type", "property_type", "country"]
+
+
+admin.site.register(Property, PropertyAdmin)
+admin.site.register(PropertyViews)
