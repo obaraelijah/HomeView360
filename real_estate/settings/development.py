71cb90370ae940a57050c4610d88fcc520ce68f8
elijah obara <elijahobara357@gmail.com>
2023-04-19 10:51:31 +0300

    jwt based authentication

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -1,5 +1,17 @@
 from .base import *
 
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_USE_TLS = True
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = "info@real-estate.com"
+DOMAIN = env("DOMAIN")
+SITE_NAME = "Real Estate"
+
+
 DATABASES = {
     "default": {
         "ENGINE": env("POSTGRES_ENGINE"),
