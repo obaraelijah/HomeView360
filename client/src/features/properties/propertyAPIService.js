aa765e527a5250d208990eb7b2a365b5ca08e2ce
elijah obara <elijahobara357@gmail.com>
2023-05-05 23:04:18 +0300

    property slide feature

---
+++ client/src/features/properties/propertyAPIService.js
@@ -0,0 +1,12 @@
+//async api calls
+import axios from "axios";
+
+//get properties
+const getProperties  = async () => {
+    const response = await axios.get("/api/v1/properties/all/");
+	return response.data;
+};
+
+const propertyAPIService = {getProperties};
+
+export default propertyAPIService;
--- client/src/features/properties/propertySlice.js
+++ client/src/features/properties/propertySlice.js
@@ -1,21 +1,56 @@
-import { createSlice} from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import propertyAPIService from "./propertyAPIService";
 
 const initialState = {
-    properties:[],
-    property:{},
-    isError:false,
-    isLoading:false,
-    isSuccess:false,
-    message: ''
-}
+	properties: [],
+	property: {},
+	isError: false,
+	isLoading: false,
+	isSuccess: false,
+	message: "",
+};
 
-export const propertySlice  = createSlice({
-    name: 'property',
-    initialState,
-    reducers: {
-        reset: (state) => initialState,
-    },
-    extraReducers: (builder) => {},
+// get all properties
+export const getProperties = createAsyncThunk(
+	"properties/getAll",
+	async (_, thunkAPI) => {
+		try {
+			return await propertyAPIService.getProperties();
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
+
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
+
+export const propertySlice = createSlice({
+	name: "property",
+	initialState,
+	reducers: {
+		reset: (state) => initialState,
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(getProperties.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(getProperties.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.properties = action.payload.results;
+			})
+			.addCase(getProperties.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+			});
+	},
 });
 
 export const { reset } = propertySlice.actions;
