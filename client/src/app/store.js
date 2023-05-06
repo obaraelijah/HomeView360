b4149d1f9107fcea7cbcbed5d3239649b37508d8
elijah obara <elijahobara357@gmail.com>
2023-05-06 12:05:13 +0300

    add login , logout, register functionality

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import propertyReducer from '../features/properties/propertySlice'
+import authReducer from "../features/properties/propertySlice"
 
 export const store = configureStore({
   reducer: {
     properties: propertyReducer,
+    auth: authReducer,
   },
 });
