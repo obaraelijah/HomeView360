7793b3b018b4398285642d8194b487def9221165
elijah obara <elijahobara357@gmail.com>
2023-05-06 12:39:43 +0300

    fixed bugs in frontend

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,21 @@
-import React  from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { store } from "./app/store";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
 
-import './index.css';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </>
+ReactDOM.render(
+	<>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</>,
+	document.getElementById("root")
 );
 
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
